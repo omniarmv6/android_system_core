92b9c9d6692fa387fe19d2ab46e7b926c8dfec70
Marcin Chojnacki <marcinch7@gmail.com>
2013-09-13 17:01:45 +0200

    init: Do not build logo.c when initlogo is disabled
    For now, logo.c was build even if TARGET_NO_INITLOGO was true.
    Changing tty to KD_GRAPHICS is now done in init.c as it should be, when
    the logo is not present.
    
    Change-Id: Ifabd9aebc9c0ad8d6903e963dfac031a6dac32e1

--- init/Android.mk
+++ init/Android.mk
@@ -10,7 +10,6 @@ LOCAL_SRC_FILES:= \
 	property_service.c \
 	util.c \
 	parser.c \
-	logo.c \
 	keychords.c \
 	signal_handler.c \
 	init_parser.c \
@@ -25,6 +24,11 @@ endif
 
 ifneq (,$(filter userdebug eng,$(TARGET_BUILD_VARIANT)))
 LOCAL_CFLAGS += -DALLOW_LOCAL_PROP_OVERRIDE=1
+endif
+
+ifneq ($(TARGET_NO_INITLOGO),true)
+LOCAL_SRC_FILES += logo.c
+LOCAL_CFLAGS    += -DINITLOGO
 endif
 
 ifeq ($(BOARD_WANTS_EMMC_BOOT),true)
